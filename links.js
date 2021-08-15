9b07abf2e945c342a2be092c37a3f31dc4ae8fc7
GabrielRalph <gltralph@gmail.com>
2021-08-15 17:27:20 +1000

    t1

---
+++ links.js
@@ -0,0 +1,6 @@
+let ReportImages = {
+  "Hanifa":"https://aphrodite.gmanetwork.com/entertainment/articles/900_675_Main_Image26_0527__20200527125333.jpg",
+  "Stefan Hauswiesner":"https://s.yimg.com/ny/api/res/1.2/NOHTWZOjrlMJygZU2gXY_Q--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTU0MDtjZj13ZWJw/https://s.yimg.com/uu/api/res/1.2/S8niplLCTo5UcAVFQMyn5g--~B/aD03MjA7dz0xMjgwO2FwcGlkPXl0YWNoeW9u/https://media.zenfs.com/en/wwd_finance_675/79528dc51af73d79a86398ba75960618",
+
+}
+export {ReportImages}
