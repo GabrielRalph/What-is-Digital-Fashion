918e176272ef6f2c40572355a4db115268f28d8f
serialvapist <gracieb29@gmail.com>
2021-08-19 14:36:49 +1000

    H

--- Resources/links.js
+++ Resources/links.js
@@ -2,10 +2,11 @@ let ReportImages = {
   "Hanifa":"https://aphrodite.gmanetwork.com/entertainment/articles/900_675_Main_Image26_0527__20200527125333.jpg",
   "Stefan Hauswiesner":"https://s.yimg.com/ny/api/res/1.2/NOHTWZOjrlMJygZU2gXY_Q--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTU0MDtjZj13ZWJw/https://s.yimg.com/uu/api/res/1.2/S8niplLCTo5UcAVFQMyn5g--~B/aD03MjA7dz0xMjgwO2FwcGlkPXl0YWNoeW9u/https://media.zenfs.com/en/wwd_finance_675/79528dc51af73d79a86398ba75960618",
   "Lola Zoido":"https://freight.cargo.site/w/350/i/347a9cd08e2cd4dcf7b346247f584300c26636e6ce8c7aac685a35f43a67f9a2/impresion-lolazoido.jpg",
-  "RalphLauren":"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/group-instore-1-1596789764.jpg",
+  "LV":"./Resources/LV_image.jpeg",
   "Stefan Hauswiesner":"https://s.yimg.com/ny/api/res/1.2/NOHTWZOjrlMJygZU2gXY_Q--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTU0MDtjZj13ZWJw/https://s.yimg.com/uu/api/res/1.2/S8niplLCTo5UcAVFQMyn5g--~B/aD03MjA7dz0xMjgwO2FwcGlkPXl0YWNoeW9u/https://media.zenfs.com/en/wwd_finance_675/79528dc51af73d79a86398ba75960618",
   "JJImage":"https://images.saatchiart.com/saatchi/388796/art/1762602/925383-DSFXZQPN-7.jpg",
   "DB":"./Resources/crack_image.jpeg",
   "Jon Jacobsen":"https://images.saatchiart.com/saatchi/388796/art/4519239/3589079-QSIZBRMH-7.jpg"
+  "LV":"https://pbs.twimg.com/media/EJs3Jg_U4AAguHY.jpg",
 }
 export {ReportImages}
