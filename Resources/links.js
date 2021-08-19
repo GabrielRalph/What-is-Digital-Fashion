07c7209c177b55872b1c338add6345cd23b84fca
GabrielRalph <gltralph@gmail.com>
2021-08-19 11:38:40 +1000

    fff

--- Resources/links.js
+++ Resources/links.js
@@ -2,11 +2,10 @@ let ReportImages = {
   "Hanifa":"https://aphrodite.gmanetwork.com/entertainment/articles/900_675_Main_Image26_0527__20200527125333.jpg",
   "Stefan Hauswiesner":"https://s.yimg.com/ny/api/res/1.2/NOHTWZOjrlMJygZU2gXY_Q--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTU0MDtjZj13ZWJw/https://s.yimg.com/uu/api/res/1.2/S8niplLCTo5UcAVFQMyn5g--~B/aD03MjA7dz0xMjgwO2FwcGlkPXl0YWNoeW9u/https://media.zenfs.com/en/wwd_finance_675/79528dc51af73d79a86398ba75960618",
   "Lola Zoido":"https://freight.cargo.site/w/350/i/347a9cd08e2cd4dcf7b346247f584300c26636e6ce8c7aac685a35f43a67f9a2/impresion-lolazoido.jpg",
-  "Jon Jacobsen":"https://jon-jacobsen.com/project/insula",
   "RalphLauren":"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.harpersbazaar.com%2Fuk%2Ffashion%2Ffashion-news%2Fa33541889%2Fralph-lauren-snapchat-partnership-bitmoji%2F&psig=AOvVaw0Xg7Jms3U_h7tcOBgBxe5I&ust=1629097797852000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKCXhvO8svICFQAAAAAdAAAAABAD",
   "Stefan Hauswiesner":"https://s.yimg.com/ny/api/res/1.2/NOHTWZOjrlMJygZU2gXY_Q--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTU0MDtjZj13ZWJw/https://s.yimg.com/uu/api/res/1.2/S8niplLCTo5UcAVFQMyn5g--~B/aD03MjA7dz0xMjgwO2FwcGlkPXl0YWNoeW9u/https://media.zenfs.com/en/wwd_finance_675/79528dc51af73d79a86398ba75960618",
   "JJImage":"https://images.saatchiart.com/saatchi/388796/art/1762602/925383-DSFXZQPN-7.jpg",
   "DB":"./Resources/crack_image.jpeg",
-  "Jon jacobsen image":"https://images.saatchiart.com/saatchi/388796/art/4519239/3589079-QSIZBRMH-7.jpg"
+  "Jon Jacobsen":"https://images.saatchiart.com/saatchi/388796/art/4519239/3589079-QSIZBRMH-7.jpg"
 }
 export {ReportImages}
